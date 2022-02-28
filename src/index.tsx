3cea79d3e46239230e11ee68a15d32618d5b4346
Fabio Milheiro <Fabio.Milheiro@redington.co.uk>
2022-02-28 18:14:27 +0000

    Firebase configurations

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDO0Gre3EQpinMVsCwqmu7IZczRzq7KWGk",
+  authDomain: "todo-27cb8.firebaseapp.com",
+  projectId: "todo-27cb8",
+  storageBucket: "todo-27cb8.appspot.com",
+  messagingSenderId: "716121129983",
+  appId: "1:716121129983:web:e807a59b3850ab9c90ddca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
